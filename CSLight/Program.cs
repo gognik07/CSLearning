f255a563ac8cbd4894cdf3ad27c5e52669fad9a6
gognik <gognik1990@gmail.com>
2022-07-29 21:19:24 +0300

    Использование переменных

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -8,8 +8,8 @@ namespace CSLight
         {
             int totalCountPictures = 52;
             int countPicturesOnLine = 3;
-            int fillingLines = 52 / 3;
-            int extraPictures = 52 % 3;
+            int fillingLines = totalCountPictures / countPicturesOnLine;
+            int extraPictures = totalCountPictures % countPicturesOnLine;
             Console.WriteLine($"Заполненных рядов картинок {fillingLines}, осталось {extraPictures} картинок");
         }
     }
