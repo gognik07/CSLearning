867532fc3ad5c0148c0872700890a217b19accae
gognik <gognik1990@gmail.com>
2022-07-29 20:43:07 +0300

    Добавьте файлы проекта.

---
+++ CSLight/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSLight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float weight = 85.3f;
+            double priceBook = 152.56;
+            bool isCheck = true;
+            String fullName = "Иванов Петр Сергеевич";
+            char firstLetterAlphabet = 'А';
+            uint age = 30;
+            byte countHands = 2;
+            long distanceToSun = 149600000;
+            int nitrogenTemperature = -196;
+            short daysInYear = 365;
+        }
+    }
+}
