ec539c9bb1e6914e66b7be64ab3a371f6eebca3d
gognik <gognik1990@gmail.com>
2022-08-04 15:34:13 +0300

    Задание с контролируемым выходом

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -6,7 +6,20 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            
+            string command;
+
+            while(true)
+            {
+                Console.Write("Введите команду: ");
+                command = Console.ReadLine();
+                Console.WriteLine($"Выполняется команда {command}...");
+                if (command == "exit")
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("До свидания!");
         }
     }
 }
