fa67cb40af173f1953986faa9bad922bdcd3b1e7
gognik <gognik1990@gmail.com>
2022-08-04 15:41:06 +0300

    Задача с последовательностью чисел

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -6,7 +6,17 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            
+            int startNumber = 5;
+            int lastNumber = 96;
+            int iterationStep = 7;
+
+            //Выбираю for, т.к. есть четкое понимание до какого числа идет последовательность
+            for(int i = startNumber; i != lastNumber; i += iterationStep)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine(lastNumber);
         }
     }
 }
