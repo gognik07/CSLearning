ae36c394cc14c8af66bb449d671ebf7f980becb8
gognik <gognik1990@gmail.com>
2022-08-04 15:51:36 +0300

    Задание с суммой последовательности

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -6,7 +6,19 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            
+            Random random = new Random();
+            int number = random.Next(0, 101);
+            int sum = 0;
+
+            Console.WriteLine($"Выбрано число {number}");
+            for(int i = 1; i <= number; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine($"Сумма чисел кратных 3 и 5 равна {sum}");
         }
     }
 }
