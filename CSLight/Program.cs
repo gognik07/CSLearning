4d2ed1dac0962182298684bc796a5f011534e242
gognik <gognik1990@gmail.com>
2022-08-04 15:30:10 +0300

    Очищение метода

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -6,16 +6,7 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            float weight = 85.3f;
-            double priceBook = 152.56;
-            bool isCheck = true;
-            String fullName = "Иванов Петр Сергеевич";
-            char firstLetterAlphabet = 'А';
-            uint age = 30;
-            byte countHands = 2;
-            long distanceToSun = 149600000;
-            int nitrogenTemperature = -196;
-            short daysInYear = 365;
+            
         }
     }
 }
