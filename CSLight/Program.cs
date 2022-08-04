84c3556d011ecef10cc705a9e574880df99a61dd
gognik <gognik1990@gmail.com>
2022-08-04 16:39:42 +0300

    Убрал магические числа

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -7,13 +7,17 @@ namespace CSLight
         static void Main(string[] args)
         {
             Random random = new Random();
-            int number = random.Next(0, 101);
+            int min = 0;
+            int max = 101;
+            int number = random.Next(min, max);
             int sum = 0;
+            int multiplicityThree = 3;
+            int multiplicityFive = 5;
 
             Console.WriteLine($"Выбрано число {number}");
             for(int i = 1; i <= number; i++)
             {
-                if (i % 3 == 0 || i % 5 == 0)
+                if (i % multiplicityThree == 0 || i % multiplicityFive == 0)
                 {
                     sum += i;
                 }
