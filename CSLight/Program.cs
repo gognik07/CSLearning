937d3418a6720918bcc0417185cd4a1ee9f2596e
gognik <gognik1990@gmail.com>
2022-08-05 15:50:00 +0300

    Задание с огранкой имени

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -6,7 +6,32 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Введите ваше имя: ");
+            string name = Console.ReadLine();
+            Console.Write("Введите символ для обвода:");
+            char characterOutline = Convert.ToChar(Console.ReadLine());
+
+            int widthOutline = 1;
+            int widthFrame = widthOutline + name.Length + widthOutline;
+            int heightOutline = 3;
+            int numberNameLine = 1;
+            for(int i = 0; i < heightOutline; i++)
+            {
+                if (i == numberNameLine)
+                {
+                    Console.WriteLine(characterOutline + name + characterOutline);
+                } 
+                else
+                {
+                    for (int j = 0; j < widthFrame; j++)
+                    {
+                        Console.Write(characterOutline);
+                    }
+                    Console.WriteLine();
+                }
+
+                
+            }
         }
     }
 }
