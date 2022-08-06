83d32a55031057fd941558f148b1ca5fd73428c5
gognik <gognik1990@gmail.com>
2022-08-06 15:05:45 +0300

    Задание с кратными числами

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -6,7 +6,23 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            
+            int minN = 1;
+            int maxN = 28;
+            Random random = new Random();
+            int n = random.Next(minN, maxN);
+            Console.WriteLine($"N = {n}");
+            int minMultiple = 100;
+            int maxMultiple = 1000;
+            int resultMultipleN = n;
+
+            while(resultMultipleN < maxMultiple)
+            {
+                if (resultMultipleN > minMultiple)
+                {
+                    Console.WriteLine(resultMultipleN);
+                }
+                resultMultipleN += n;
+            }
         }
     }
 }
