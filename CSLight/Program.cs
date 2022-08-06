d57f717160cf318383a68df1b19a6320ef16270b
gognik <gognik1990@gmail.com>
2022-08-06 15:16:34 +0300

    Задание два в степени

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -6,7 +6,22 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            
+            Random random = new Random();
+            int minNumber = 1;
+            int maxNumber = 100;
+            int comparedNumber = random.Next(minNumber, maxNumber);
+            int powerTwo = 1;
+            int resultPowerTwo = 2;
+
+            while(resultPowerTwo <= comparedNumber)
+            {
+                resultPowerTwo *= 2;
+                powerTwo++;
+            }
+
+            Console.WriteLine($"Выбранное число = {comparedNumber}, " +
+                $"полученная степень двойки - {powerTwo}, " +
+                $"два в степень {powerTwo} - {resultPowerTwo}");
         }
     }
 }
