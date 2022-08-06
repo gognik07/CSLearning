b34d3c1e96d4c6bc03c69cc7163eda3ad6d2ceb3
gognik <gognik1990@gmail.com>
2022-08-06 10:36:28 +0300

    Убрал внешний цикл

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -11,26 +11,16 @@ namespace CSLight
             Console.Write("Введите символ для обвода:");
             char characterOutline = Convert.ToChar(Console.ReadLine());
 
-            int widthOutline = 1;
-            int widthFrame = widthOutline + name.Length + widthOutline;
-            int heightOutline = 3;
-            int numberNameLine = 1;
-            for(int i = 0; i < heightOutline; i++)
+            string nameWithFrame = characterOutline + name + characterOutline;
+            for(int i = 0; i < nameWithFrame.Length; i++)
             {
-                if (i == numberNameLine)
-                {
-                    Console.WriteLine(characterOutline + name + characterOutline);
-                } 
-                else
-                {
-                    for (int j = 0; j < widthFrame; j++)
-                    {
-                        Console.Write(characterOutline);
-                    }
-                    Console.WriteLine();
-                }
-
-                
+                Console.Write(characterOutline);
+            }
+            Console.WriteLine();
+            Console.WriteLine(characterOutline + name + characterOutline);
+            for (int i = 0; i < nameWithFrame.Length; i++)
+            {
+                Console.Write(characterOutline);
             }
         }
     }
