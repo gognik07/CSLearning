b8c73f44fecfa3bb13630e0517f9e2f5a7b7dd96
gognik <gognik1990@gmail.com>
2022-08-06 11:47:18 +0300

    Задание с паролем и секретной фразой

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -6,7 +6,27 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            
+            string secretMessage = "Копай на восток до обеда!";
+            string password = "1qaz2WSX";
+            int countAttempts = 3;
+            bool hasAccess = false;
+
+            for(int i = 0; i < countAttempts; i++)
+            {
+                Console.Write("Введите пароль: ");
+                string userInput = Console.ReadLine();
+                
+                if(userInput == password)
+                {
+                    Console.WriteLine($"Ваше тайное сообщение: {secretMessage}");
+                    break;
+                }
+                else
+                {
+                    int remainingAttempts = countAttempts - i - 1;
+                    Console.WriteLine($"Пароль не верный. Осталось {remainingAttempts} попыток");
+                }
+            }
         }
     }
 }
