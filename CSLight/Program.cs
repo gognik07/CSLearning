f064d044db377e7c21c396cf74343a4348842323
gognik <gognik1990@gmail.com>
2022-08-09 20:23:30 +0300

    Задание со скобочным выражением

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -6,7 +6,49 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Введите скобочное выражение:");
+            string bracketExpression = Console.ReadLine();
+            int countOpenedBrackets = 0;
+            int nestingDepth = 0;
+            bool isCorrectExpression = true;
+
+            for(int i = 0; i < bracketExpression.Length; i++)
+            {
+                if (bracketExpression[i] == '(')
+                {
+                    countOpenedBrackets++;
+                }
+                else if (bracketExpression[i] == ')')
+                {
+                    countOpenedBrackets--;
+                }
+                else
+                {
+                    Console.WriteLine($"Недопустимый символ в скобочном выражении {bracketExpression[i]}");
+                    isCorrectExpression = false;
+                    break;
+                }
+
+                if (countOpenedBrackets < 0)
+                {
+                    isCorrectExpression = false;
+                    break;
+                }
+
+                if (countOpenedBrackets > nestingDepth)
+                {
+                    nestingDepth = countOpenedBrackets;
+                }
+            }
+
+            if (countOpenedBrackets == 0 && isCorrectExpression)
+            {
+                Console.WriteLine($"Строка корректная и максимум глубины равняется {nestingDepth}");
+            }
+            else
+            {
+                Console.WriteLine("Не корректное выражение");
+            }
         }
     }
 }
