46a4d94f9b35be1f36156e17fd75f465d366d2db
gognik <gognik1990@gmail.com>
2022-08-21 19:20:19 +0300

    Задание по работе со столбцами и строками массива

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -6,7 +6,30 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            
+            int[,] arrayForCalculation = { 
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 } 
+            };
+            int countRowForSum = 1;
+            int sumRow = 0;
+
+            for (int i = 0; i < arrayForCalculation.GetLength(0); i++)
+            {
+                sumRow += arrayForCalculation[countRowForSum, i];
+            }
+
+            Console.WriteLine($"Сумма 2-ой строки равна {sumRow}");
+
+            int countColForMultiplication = 0;
+            int multiplicationCol = 1;
+
+            for (int i = 0; i < arrayForCalculation.GetLength(1); i++)
+            {
+                multiplicationCol *= arrayForCalculation[i, countColForMultiplication];
+            }
+
+            Console.WriteLine($"Произведение первого столбца равно {multiplicationCol}");
         }
     }
 }
