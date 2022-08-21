5d9827b07553ee207518a79634e27b74d2ba07b9
gognik <gognik1990@gmail.com>
2022-08-21 20:54:05 +0300

    Задача с поиском локальных максимумов

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -6,7 +6,44 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            
+            int[] arrayForSearching = new int[30];
+            Random random = new Random();
+            int minRandom = 0;
+            int maxRandom = 100;
+            int indexFirstElement = 0;
+            int indexLastElement = arrayForSearching.GetLength(0) - 1;
+
+            for (int i = 0; i < arrayForSearching.GetLength(0); i++)
+            {
+                arrayForSearching[i] = random.Next(minRandom, maxRandom);
+                Console.Write(arrayForSearching[i] + " | ");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < arrayForSearching.GetLength(0); i++)
+            {
+                if (i == indexFirstElement)
+                {
+                    if (arrayForSearching[i] >= arrayForSearching[i + 1])
+                    {
+                        Console.WriteLine($"Локальный максимум {arrayForSearching[i]} в позиции {i}");
+                    }
+                } else if (i == indexLastElement)
+                {
+                    if (arrayForSearching[i] >= arrayForSearching[i - 1])
+                    {
+                        Console.WriteLine($"Локальный максимум {arrayForSearching[i]} в позиции {i}");
+                    }
+                } else
+                {
+                    if (arrayForSearching[i] >= arrayForSearching[i + 1] && arrayForSearching[i] >= arrayForSearching[i - 1])
+                    {
+                        Console.WriteLine($"Локальный максимум {arrayForSearching[i]} в позиции {i}");
+                    }
+                }
+            }
+
         }
     }
 }
