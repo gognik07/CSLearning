c664891f6eacc3ce7104ac3947b46aa26d488966
gognik <gognik1990@gmail.com>
2022-08-21 19:32:52 +0300

    Задание с расчетом сумм введенных чисел

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -6,7 +6,47 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            
+            int[] arrayForSum = new int[0];
+            int arrayIncrement = 1;
+            bool isWorked = true;
+
+            while(isWorked)
+            {
+                Console.Write("Введите число для будущего сложения или команду sum или exit: ");
+                string command = Console.ReadLine();
+
+                switch(command)
+                {
+                    case "exit":
+                        isWorked = false;
+                        break;
+                    case "sum":
+                        int sum = 0;
+
+                        for(int i = 0; i < arrayForSum.GetLength(0); i++)
+                        {
+                            sum += arrayForSum[i];
+                        }
+
+                        Console.WriteLine($"Сумма введенных чисел равна {sum}");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    default:
+                        int[] tempArrayForSum = new int[arrayForSum.GetLength(0) + arrayIncrement];
+
+                        for(int i = 0; i < arrayForSum.GetLength(0); i++)
+                        {
+                            tempArrayForSum[i] = arrayForSum[i];
+                        }
+
+                        tempArrayForSum[arrayForSum.GetLength(0)] = Convert.ToInt32(command);
+                        arrayForSum = tempArrayForSum;
+
+                        Console.Clear();
+                        break;
+                }
+            }
         }
     }
 }
