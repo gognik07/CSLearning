6f225096346d9d644f8203d67e94a30ceef7718d
gognik <gognik1990@gmail.com>
2022-08-23 20:33:52 +0300

    Задание с сортировкой массива

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -6,7 +6,37 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            
+            int[] sortedArray = new int[20];
+            Random random = new Random();
+            int minRandom = 0;
+            int maxRandom = 100;
+
+            Console.WriteLine("Неотсортированный массив:");
+
+            for (int i = 0; i < sortedArray.GetLength(0); i++)
+            {
+                sortedArray[i] = random.Next(minRandom, maxRandom);
+                Console.Write(sortedArray[i] + " | ");
+            }
+
+            for (int i = 0; i < sortedArray.GetLength(0) - 1; i++)
+            {
+                for (int j = i; j < sortedArray.GetLength(0); j++)
+                {
+                    if (sortedArray[i] > sortedArray[j])
+                    {
+                        int currentStorageForChangedElement = sortedArray[i];
+                        sortedArray[i] = sortedArray[j];
+                        sortedArray[j] = currentStorageForChangedElement;
+                    }
+                }
+            }
+
+            Console.WriteLine("\nОтсортированный массив:");
+            for (int i = 0; i < sortedArray.GetLength(0); i++)
+            {
+                Console.Write(sortedArray[i] + " | ");
+            }
         }
     }
 }
