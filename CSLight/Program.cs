3bb5f3585a589346f845da3b46f4d273930b44e5
gognik <gognik1990@gmail.com>
2022-08-23 20:39:56 +0300

    Задание с разделением текста на массив

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -6,7 +6,14 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            
+            string textForSpliting = "Lorem ipsum dolor sit amet consectetur adipiscing elit";
+            char separator = ' ';
+            string[] arrayFromText = textForSpliting.Split(separator);
+
+            for(int i = 0; i < arrayFromText.GetLength(0); i++)
+            {
+                Console.WriteLine(arrayFromText[i]);
+            }
         }
     }
 }
