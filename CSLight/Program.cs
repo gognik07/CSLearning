1307b9c8e62181d00ecea9c61c81c40f32044f6b
gognik <gognik1990@gmail.com>
2022-09-04 19:00:51 +0300

    Задание с вводом числа

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -6,7 +6,21 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
+            ReadInt();
+        }
+
+        private static void ReadInt()
+        {
+            bool isCorrectNumber = false;
+            int inputNumber = 0;
             
+            while(isCorrectNumber == false)
+            {
+                Console.Write("Введите число: ");
+                isCorrectNumber = int.TryParse(Console.ReadLine(), out inputNumber);
+            }
+
+            Console.WriteLine("Введено число: " + inputNumber);
         }
     }
 }
