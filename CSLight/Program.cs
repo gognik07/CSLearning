52cc028bbebecde19a5c13ae705f65e0c2510c86
gognik <gognik1990@gmail.com>
2023-02-21 21:53:55 +0300

    Задача с подсчетом суммы в динамическом массиве

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CSLight
 {
@@ -6,7 +7,43 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            
+            bool isWorked = true;
+            List<int> listNumbers = new List<int>();
+
+            while(isWorked)
+            {
+                Console.Write("Введите число: ");
+                string command = Console.ReadLine();
+                int number;
+
+                if (command == "sum")
+                {
+                    isWorked = false;
+                } else if (int.TryParse(command, out number))
+                {
+                    listNumbers.Add(number);
+                } else
+                {
+                    Console.WriteLine("Введено не число");
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+
+            int sum = CalculateSum(listNumbers);
+            Console.WriteLine($"Полученная сумма: {sum}");
+        }
+
+        private static int CalculateSum(List<int> listNumbers)
+        {
+            int sum = 0;
+
+            foreach(int number in listNumbers) {
+                sum += number;
+            }
+
+            return sum;
         }
     }
 }
