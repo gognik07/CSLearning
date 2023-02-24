6062b67fc2ceec1d97a1360f556955adc3f1bdcd
gognik <gognik1990@gmail.com>
2023-02-24 16:26:52 +0300

    Задание с объединением списков

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CSLight
 {
@@ -6,7 +7,38 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            
+            List<string> firstList = new List<string>() { "1", "3",  "4"};
+            Console.WriteLine("Первый список:");
+            PrintList(firstList);
+            List<string> secondList = new List<string>() { "1", "2",  "4"};
+            Console.WriteLine("Второй список:");
+            PrintList(secondList);
+            List<string> unionedList = UnionLists(firstList, secondList);
+            Console.WriteLine("Объединенный список:");
+            PrintList(unionedList);
+        }
+
+        private static void PrintList(List<string> printedList)
+        {
+            foreach(string value in printedList)
+            {
+                Console.WriteLine(value);
+            }
+        }
+
+        private static List<string> UnionLists(List<string> firstList, List<string> secondList)
+        {
+            List<string> unionedList = new List<string>(firstList);
+
+            foreach(string value in secondList)
+            {
+                if (unionedList.Contains(value) == false)
+                {
+                    unionedList.Add(value);
+                }
+            }
+
+            return unionedList;
         }
     }
 }
