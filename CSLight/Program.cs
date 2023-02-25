ce1de39e5362e4a68318f84f71d7c823884d9d69
gognik <gognik1990@gmail.com>
2023-02-25 20:27:14 +0300

    Добавление логики на уникальность всех списков

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -7,10 +7,10 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            List<string> firstList = new List<string>() { "1", "3",  "4"};
+            List<string> firstList = new List<string>() { "1", "3",  "4", "1", "5"};
             Console.WriteLine("Первый список:");
             PrintList(firstList);
-            List<string> secondList = new List<string>() { "1", "2",  "4"};
+            List<string> secondList = new List<string>() { "1", "2",  "4", "6", "5", "5"};
             Console.WriteLine("Второй список:");
             PrintList(secondList);
             List<string> unionedList = UnionLists(firstList, secondList);
@@ -28,17 +28,23 @@ namespace CSLight
 
         private static List<string> UnionLists(List<string> firstList, List<string> secondList)
         {
-            List<string> unionedList = new List<string>(firstList);
+            List<string> unionedList = new List<string>();
 
-            foreach(string value in secondList)
-            {
-                if (unionedList.Contains(value) == false)
-                {
-                    unionedList.Add(value);
-                }
-            }
+            AddUniqueValuesFromList(ref unionedList, firstList);
+            AddUniqueValuesFromList(ref unionedList, secondList);
 
             return unionedList;
+        }
+
+        private static void AddUniqueValuesFromList(ref List<string> finalList, List<string> addedList)
+        {
+            foreach(string value in addedList)
+            {
+                if (finalList.Contains(value) == false)
+                {
+                    finalList.Add(value);
+                }
+            }
         }
     }
 }
